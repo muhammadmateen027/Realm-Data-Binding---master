0766cceddfb3e0102388190452b2194aca19f9ba
Mateen <mateenumt027@gmail.com>
2017-09-24 21:00:28 +0500

    Ddetail activity designed...

--- app/src/main/java/com/on2sol/logbook/Activities/DetailActivity.java
+++ app/src/main/java/com/on2sol/logbook/Activities/DetailActivity.java
@@ -2,6 +2,10 @@ package com.on2sol.logbook.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.on2sol.logbook.R;
 
@@ -11,5 +15,16 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView back = (ImageView) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
